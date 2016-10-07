adb88d5b3cd14ab1ae25aa829e67e34db1f1a3a4
Mary Elaine <marymahaffey1@gmail.com>
2016-10-07 17:01:36 -0400

    All tests fixec for carrier wave

--- spec/features/profile_photo_spec.rb
+++ spec/features/profile_photo_spec.rb
@@ -8,20 +8,20 @@ feature 'profile picture' do
       visit root_path
       click_link 'Sign Up'
       fill_in 'Username', with: 'newUser'
-      attach_file "user_profile_picture", "#{Rails.root}/spec/support/images/photo.png"
+      attach_file 'user_profile_picture', "#{Rails.root}/spec/support/images/LaunchHalloween16.jpg"
       fill_in 'Email', with: 'email@gmail.com'
       fill_in 'Password', with: 'password'
       fill_in 'Password confirmation', with: 'password'
       click_button 'Sign up'
-
       expect(page).to have_xpath(
-    "//img[contains(@src,'/images/default_profile_picture.png')]")
+      "//img[contains(@src,'LaunchHalloween16.jpg')]"
+    )
       expect(page).to have_content('Welcome! You have signed up successfully.')
       expect(page).to have_content('Sign Out')
     end
   end
 
-  context 'A a user I visit my profile page' do
+  context 'A a user I visit my edit profile page' do
     scenario 'user uploads a profile picture successfully' do
       visit root_path
       click_link 'Sign In'
@@ -30,6 +30,7 @@ feature 'profile picture' do
       click_button 'Log in'
       click_link 'Your Profile'
       attach_file 'user_profile_picture', "#{Rails.root}/spec/support/images/photo.png"
+
       click_button 'Update'
 
       expect(page).to have_xpath(

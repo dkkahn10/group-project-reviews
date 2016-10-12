365b00580f28b3fcc25095d91b3975fec731bc20
Dustin Kahn <dustin@dustinkahn.com>
2016-10-12 09:05:22 -0400

    added votes_total to review table

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :location
   belongs_to :user
   validates :intimacy_rating, numericality: {only_interger: true}, inclusion: {in: 1..5, message: "Rating must be between 1 - 5"}
-  validates :upvote_count, presence: true
-  validates :downvote_count, presence: true
+  validates :vote_total, presence: true 
   has_many :votes
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007180417) do
+ActiveRecord::Schema.define(version: 20161012130025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,11 @@ ActiveRecord::Schema.define(version: 20161007180417) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "intimacy_rating", null: false
+    t.integer "intimacy_rating",             null: false
     t.string  "reasoning"
     t.integer "location_id"
     t.integer "user_id"
+    t.integer "votes_total",     default: 0, null: false
     t.index ["location_id"], name: "index_reviews_on_location_id", using: :btree
     t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end

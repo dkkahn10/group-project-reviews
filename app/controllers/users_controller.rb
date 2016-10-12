5c4eb93d873d10bf91072591f72d3d242719dc42
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-10-12 16:46:45 -0400

    fixed userphoto update error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,18 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def destroy
-    @user = User.find(params[:id])
-
-    if authorized_user
-      @user.destroy
-      redirect_to root_path
-    end
-  end
-
-  def delete
-    sign_out_and_redirect(current_user)
-  end
+  # def destroy
+  #   @user = User.find(params[:id])
+  #
+  #   if authorized_user
+  #     @user.destroy
+  #     redirect_to root_path
+  #   end
+  # end
+  #
+  # def delete
+  #   sign_out_and_redirect(current_user)
+  # end
 
   private
 

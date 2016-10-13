5a38d43e2a9be4ae11a1bced7200ae4e2cbaa62c
Dustin Kahn <dustin@dustinkahn.com>
2016-10-13 11:47:47 -0400

    config line revert

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module GroupReviewSite
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.

8dca979d90998105f17065ac2def063f7ad7ce2e
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-10-13 09:19:34 -0400

    passing tests checking for codeship

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,9 @@
-class Users
-  class RegistrationsController < Devise::RegistrationsController
-    def destroy
-      resource.soft_delete
-      Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-      set_flash_message :notice, :destroyed if is_flashing_format?
-      yield resource if block_given?
-      respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
-    end
+class Users::RegistrationsController < Devise::RegistrationsController
+  def destroy
+    resource.soft_delete
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed if is_flashing_format?
+    yield resource if block_given?
+    respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
   end
 end

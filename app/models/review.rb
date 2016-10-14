1f4cfd23b7b06541e6b0a87c19f669b6eb447aa5
Amanda Beiner <aebeiner@gmail.com>
2016-10-14 10:17:22 -0400

    final styling changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :location
   belongs_to :user
-  validates :intimacy_rating, numericality: {only_interger: true}, inclusion: {in: 1..5, message: "Rating must be between 1 - 5"}
+  validates :intimacy_rating, numericality: {only_integer: true}, inclusion: {in: 1..5, message: "Rating must be between 1 - 5"}
   validates :votes_total, presence: true
   has_many :votes
   belongs_to :user

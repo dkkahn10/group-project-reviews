d3fc4099714873c0f9f5d50c047bc2302e49d8dc
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-10-14 08:07:38 -0400

    development/mailcatcher config

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,7 +1,7 @@
 class Mailer < ApplicationMailer
   def new_review(review)
-    @review = review
 
+    @review = review
     mail(
       to: review.location.user.email,
       subject: "New Review for #{review.location.name_of_location}")
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
 
   config.cache_classes = false
-  
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -55,4 +57,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
